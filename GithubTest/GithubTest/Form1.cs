d5eb26ed396e6f893b6b6becda5f8d4ad415c063
sen.takematsu <sen.takematsu@dkn.co.jp>
2019-02-22 10:35:05 +0900

    コミット

--- GithubTest/GithubTest/Form1.cs
+++ GithubTest/GithubTest/Form1.cs
@@ -16,5 +16,10 @@ namespace GithubTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("test");
+        }
     }
 }
